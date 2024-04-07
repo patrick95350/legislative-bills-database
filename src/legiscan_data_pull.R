08bc5e8c44d60af1668f573e9e6296292dbce416
Patrick Rogers <patrick.rogers@library.ca.gov>
2024-04-07 11:56:33 -0700

    Initial Commit

---
+++ src/legiscan_data_pull.R
@@ -0,0 +1,44 @@
+### ASM Rodriguez Budget Wins
+#     Script to load LegInfo data and search it form Freddie Rodriguez budget wins
+#
+#     By Patrick Rogers, California Research Bureau
+#       April 2024
+#
+#     Uses the following Packages
+#       RSQLite
+#
+#     Uses the following data
+#       
+
+# To Do
+#   1. Item 1
+#   2. Item 2
+
+# Clear the workspace
+remove(list=ls(all=TRUE))
+
+# Load Packages
+library(here)
+
+library(htt
+
+# Inits
+setwd(here::here())
+
+
+# Load Sourcefiles
+
+# User Parameters
+
+# Custom Functions ####
+
+
+# Get LegiScan Data ####
+
+legiscan_dbs <- 
+
+
+
+# Step 2 ####
+
+# Step 3 ####
