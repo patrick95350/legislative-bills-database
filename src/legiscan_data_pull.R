dccebc220628e364c682edc00709b8b698fa0370
Patrick Rogers <patrick.rogers@library.ca.gov>
2024-04-12 11:00:52 -0700

    Punted to get request done in time

--- src/legiscan_data_pull.R
+++ src/legiscan_data_pull.R
@@ -175,82 +175,125 @@ for(i in seq_along(legiscan_budget_bills)){
   setTxtProgressBar(progress_bar, i)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-# Download HTML versions of chaptered budget bills
+# Download HTML versions of chaptered budget bills ####
 # Create directory to save local copies of downloaded bills
 path <- here::here('downloads', 'chaptered_budget_bills')
 if(!dir.exists(path)){
   dir.create(path)
 }
 # Drop bills that don't have a chaptered version
-chaptered_budget_bills_list <- lapply(legiscan_budget_bills, \(x) lapply(x, \(x) "Chaptered" %in% x$texts$type))
+chaptered_budget_bills_list <- lapply(legiscan_budget_bills, \(x) lapply(x, \(x) 'Chaptered' %in% x$texts$type))
+
+progress_bar <- txtProgressBar(min=1,max=length(legiscan_budget_bills), style=3)
+setTxtProgressBar(progress_bar, 1)
 
 for(i in seq_along(legiscan_budget_bills)){
-  chaptered_bills_this_year <- legiscan_budget_bills[[1]][unlist(chaptered_budget_bills_list[[1]])]
+  current_year <- names(legiscan_budget_bills)[i]
+  # Get a list of the chaptered budget bills for current year
+  chaptered_bills_this_year <- legiscan_budget_bills[[i]][unlist(chaptered_budget_bills_list[[i]])]
   
   for(j in seq_along(chaptered_bills_this_year)){
-    # add check for local files
-    
-    request <- httr::GET('https://api.legiscan.com/',
-                         query = list(key = legiscan_api_key,
-                                      op = 'getDataSetList',
-                                      state = 'CA'))
-    
-    url <- https://api.legiscan.com/?key=
-      
-      &op=getBillText&id=2976729
-  }
-  
-  
-  
-  
-}
-
-
-
-
-
-chaptered_budget_bills_list <- legiscan_budget_bills[chaptered_budget_bills_list]
-# Clean up spaces, split the year and bill number and use  those to generate Legiscan links
-chaptered_budget_bills_list <- gsub(" ", '', names(chaptered_budget_bills_list)[chaptered_budget_bills_list])
-chaptered_budget_bills_list <- strsplit(chaptered_budget_bills_list, '.', fixed = TRUE)
-chaptered_budget_bills_list <- unlist(lapply(chaptered_budget_bills_list,
-                                             \(x) paste0('https://legiscan.com/CA/text/', x[2], '/', x[1])))
-
-progress_bar <- txtProgressBar(min=1,max=length(chaptered_budget_bills_list), style=3)
-
-//*[@id="bill"]
-
-for(i in seq_along(chaptered_budget_bills_list)){
-  print(i)
-  # Check if we've got all the files downloaded so we don't over-tax Legiscan
-  if(!(length(list.files(path)) == length(chaptered_budget_bills_list)) | force_download_legiscan){
-    # Get the HTML of the main Legiscan page for the bill and extract the link to the chaptered version
-    bill_nodes  <- xml2::read_html(chaptered_budget_bills_list[i])
-    bill_text <- xml_find_all(bill_nodes, "//*[@id='bill']")
-    bill_filename <- xml_find_all(bill_nodes, "//div[@id='gaits-wrapper']//a")
-    bill_filename <- xml_text(bill_filename[grep('-Chaptered\\.html', xml_text(bill_filename))])
-    
-    if(length(bill_text) == 1 & length(bill_filename) == 1){
-      write_xml(bill_text,
-                file = here::here(path, bill_filename))
-    } else {
-      warning('Could not find chaptered bill for ', chaptered_budget_bills_list[i], '.')
+    current_bill <- gsub(' ', '',names(chaptered_bills_this_year)[j])
+    # Check if we've got all the files downloaded so we don't over-tax Legiscan
+    if(!(length(list.files(path)) == sum(unlist(chaptered_budget_bills_list))) | force_download_legiscan){
+      # Name the HTML files  with the year and bill number
+      html_file <- paste0(current_year, '_', current_bill, '.html')
+      # Get the doc id from the texts list from each bill, we want the last id, because that's the chaptered version
+      doc_id <- chaptered_bills_this_year[[j]]$texts$doc_id
+      doc_id <- doc_id[length(doc_id)]
+      # Send an API call to LegiScan to get the bill text
+      request <- httr::GET('https://api.legiscan.com/',
+                           query = list(key = legiscan_api_key,
+                                        op = 'getBillText',
+                                        id = doc_id))
+      # Extract the content from the response, decode it and write it as a bbinary to the HTML file
+      writeBin(base64_dec(httr::content(request)$text$doc), here::here(path, html_file))
     }
   }
-  Sys.sleep(.01)
-  #setTxtProgressBar(progress_bar, i)
+  setTxtProgressBar(progress_bar, i)
 }
+
+# Parse HTML to extract budget items ####
+path <- here::here('downloads', 'chaptered_budget_bills')
+budget_HTML_files_list <- list.files(path = path)
+
+#budget_bill_appropriations <- vector(mode = 'list', length = length(budget_HTML_files_list))
+pomona_bills <- vector(mode = 'list', length = length(budget_HTML_files_list))
+
+# Loop over each bill and parse
+for(i in seq_along(budget_HTML_files_list)){
+  
+  # Read  in HTML and extract text
+  current_bill_text <- xml2::read_html(here::here(path, budget_HTML_files_list[i]), encoding = 'UTF-8', options = c('IGNORE_ENC')) |> xml2::xml_text()
+  
+  # Check if there are appropriations
+  # if(!grepl('Appropriation: yes', current_bill_text)){
+  #   #budget_bill_appropriations[[i]] <- NULL
+  #   message('No appropriations found in ', budget_HTML_files_list[i], '.')
+  # } else {
+  #   message('Appropriations found in ', budget_HTML_files_list[i], '.')
+  # }
+  # Handle carriage returns and tabs
+  #current_bill_text <- unlist(strsplit(current_bill_text, split = '\\n|\\t'))
+  
+  # Remove spaces and blanks
+  #current_bill_text <- current_bill_text[!current_bill_text=='']
+  #current_bill_text <- gsub('^ +', '', current_bill_text)
+  
+  # Drop Counsel's Digest
+  #start_of_bill_pattern <- 'THE PEOPLE OF THE STATE OF CALIFORNIA DO ENACT AS FOLLOWS|Resolved by the Senate, the Assembly concurring|Resolved by the Assembly, the Senate concurring'
+  #current_bill_text <- current_bill_text[(grep(start_of_bill_pattern, current_bill_text) + 1 ):length(current_bill_text)]
+  
+  # Add extra blank at end of bill for looping
+  #current_bill_text <- c(current_bill_text, '')
+  
+  # Remove statutory sections
+  
+  # Condense listed items into single line
+  # Patterns
+  #   '^SEC[T\\.]I?O?N? [0-9]+' matches 'SECTION' and 'SEC.' headings
+  #   '^\\(\\w+?\\)' matches '(a) (1) (A)' headings
+  #   '^[0-9]+\\.  ' matches '69453.  ' statute headings
+  # headings_pattern <- '^SEC[T\\.]I?O?N? [0-9]+|^\\(\\w+?\\)|^[0-9]+\\.'
+  # line_item_index <- grep(headings_pattern, current_bill_text)
+  # 
+  # for(j in  seq_along(line_item_index)){
+  #   k <- line_item_index[j] + 1
+  #   while(!grepl(headings_pattern, current_bill_text[k]) & !(current_bill_text[k] == '')){
+  #     current_bill_text[line_item_index[j]] <- paste(current_bill_text[line_item_index[j]], current_bill_text[k])
+  #     current_bill_text[k] <- ''
+  #     k <- k+1
+  #   }
+  # }
+  
+  # Search for POMONA in the text
+  pomona_search <- grepl('POMONA', current_bill_text, ignore.case = TRUE)
+  
+  pomona_bills[[i]] <- sum(pomona_search)
+  
+  # ### STOPPED HERE IDENTIFYING INDIVIDUAL ITEMS, SWITCHING TO JUST SEARCHING FOR POMONA
+  # 
+  # # Process line items with dollar appropriations
+  # # Find broad appropriations that aren't broken down, or just the broken out items
+  # line_item_index <- which((grepl('The sum of .+? dollars \\(\\$[0-9,]+\\) is hereby appropriated', current_bill_text) &
+  #                             !grepl('allocation pursuant to the following schedule', current_bill_text)) |
+  #                            grepl('^\\(\\w+?\\) .+?\\$[0-9,]+', current_bill_text))
+  # current_bill_line_items <- vector(mode = 'list', length = length(line_item_index))
+  # 
+  # for(j in seq_along(line_item_index)){
+  #   current_bill_line_items[[j]] <- c(gsub('^\\(\\w+?\\) (.+?) +\\$[0-9\\,]+', '\\1', current_bill_text[line_item_index[j]]),
+  #                                     gsub('^\\(\\w+?\\) .+? +(\\$[0-9\\,]+) *.*$', '\\1', current_bill_text[line_item_index[j]]))
+  # }
+  # 
+  # temp <- do.call(rbind.data.frame, current_bill_line_items)
+  # colnames(temp)  <- c('desc', 'amount')
+  # temp$bill <- budget_HTML_files_list[i]
+  # 
+  # budget_bill_appropriations[[i]] <- temp
+  
+  
+}
+
 
 
 
